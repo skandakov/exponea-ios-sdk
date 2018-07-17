03eefe3f86a778c1bde92fe20d0362056b912988
a.skandakov <skandakov@list.ru>
2018-07-17 20:33:28 +0300

    get automaticPushNotificationTracking from plist

--- ExponeaSDK/ExponeaSDK/Classes/Models/Configuration.swift
+++ ExponeaSDK/ExponeaSDK/Classes/Models/Configuration.swift
@@ -123,6 +123,10 @@ public struct Configuration: Decodable {
         if let automaticSessionTracking = try container.decodeIfPresent(Bool.self, forKey: .automaticSessionTracking) {
             self.automaticSessionTracking = automaticSessionTracking
         }
+        
+        if let automaticPushNotificationTracking = try container.decodeIfPresent(Bool.self, forKey: .automaticPushNotificationTracking) {
+            self.automaticPushNotificationTracking = automaticPushNotificationTracking
+        }
     }
 }
 
